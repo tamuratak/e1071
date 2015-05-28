465ff34fad3de9fe4012a92bcd9e804a387c449b
Takashi Tamura <tamuratak@users.noreply.github.com>
2015-05-28 12:26:45 +0900

    add openmp pragmas

--- src/svm.cpp
+++ src/svm.cpp
@@ -473,7 +473,7 @@ void Solver::reconstruct_gradient()
 
 	int i,j;
 	int nr_free = 0;
-
+#pragma omp parallel for private(j)
 	for(j=active_size;j<l;j++)
 		G[j] = G_bar[j] + p[j];
 
@@ -1288,6 +1288,7 @@ public:
 		int start, j;
 		if((start = cache->get_data(i,&data,len)) < len)
 		{
+#pragma omp parallel for private(j)
 			for(j=start;j<len;j++)
 				data[j] = (Qfloat)(y[i]*y[j]*(this->*kernel_function)(i,j));
 		}
@@ -1337,6 +1338,7 @@ public:
 		int start, j;
 		if((start = cache->get_data(i,&data,len)) < len)
 		{
+#pragma omp parallel for private(j)
 			for(j=start;j<len;j++)
 				data[j] = (Qfloat)(this->*kernel_function)(i,j);
 		}
@@ -1403,6 +1405,7 @@ public:
 		int j, real_i = index[i];
 		if(cache->get_data(real_i,&data,l) < l)
 		{
+#pragma omp parallel for private(j)
 			for(j=0;j<l;j++)
 				data[j] = (Qfloat)(this->*kernel_function)(real_i,j);
 		}
@@ -2517,6 +2520,7 @@ double svm_predict_values(const svm_model *model, const svm_node *x, double* dec
 	{
 		double *sv_coef = model->sv_coef[0];
 		double sum = 0;
+#pragma omp parallel for private(i) reduction(+:sum)
 		for(i=0;i<model->l;i++)
 			sum += sv_coef[i] * Kernel::k_function(x,model->SV[i],model->param);
 		sum -= model->rho[0];
@@ -2533,6 +2537,7 @@ double svm_predict_values(const svm_model *model, const svm_node *x, double* dec
 		int l = model->l;
 		
 		double *kvalue = Malloc(double,l);
+#pragma omp parallel for private(i)
 		for(i=0;i<l;i++)
 			kvalue[i] = Kernel::k_function(x,model->SV[i],model->param);
 
